28544825e9d15542da360e8911713fde8a9b0616
99002631 <71481233+99002631@users.noreply.github.com>
2020-09-19 10:28:21 +0530

    Create main.c
    main file created

---
+++ main.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tBasic C Program demonstrartion\n\n\n");
+    int choice, num, i;
+    unsigned long int fact;
+
+    while(1)
+    {
+        printf("1. Area of circle \n2. Area of triangle\n3. Area of square\n4. Exit\n\n\n");
+        printf("Enter your choice :  ");
+        scanf("%d",&choice);
+        
+        switch(choice)
+        {
+            case 1:
+                  int radius;
+                  printf("Enter the radius of circle:  ");
+                  scanf("%d",&radius);
+                  area_of_circle(radius);
+                  break;
+            case 2:
+                  int base,height;
+                  printf("Enter base length of triangle:  ");
+                  scanf("%d",&base);
+                  printf("Enter height of triangle:  ");
+                  scanf("%d",&height);
+                  area_of_triangle(base,height);
+                  break;
+            case 3:
+                  int side_length;
+                  printf("Enter the side length of square:  ");
+                  scanf("%d",&side_length);
+                  area_of_square(side_length);
+                  break;
+        
+            case 4:
+                printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+                exit(0);    // terminates the complete program execution
+        }
+    }
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
