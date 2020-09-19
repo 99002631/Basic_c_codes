59672d7d690c09db57e2766f13315c96ac687f37
99002631 <71481233+99002631@users.noreply.github.com>
2020-09-19 10:44:31 +0530

    Create function_def.c

---
+++ src/function_def.c
@@ -0,0 +1,16 @@
+#include <function_def.h>
+
+float area_of_circle(int radius)
+  { 
+    return (3.14*radius*radius);
+  }
+
+float area_of_triangle(int base, int height)
+  { 
+    return (3.14*r*r);
+  }
+
+int area_of_square(int side_length)
+  { 
+    return (side_length*side_length);
+  }
