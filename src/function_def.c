add5e5e36c03a58adcadeb7b5fb527d9a7a05918
99002631 <71481233+99002631@users.noreply.github.com>
2020-09-19 11:37:39 +0530

    Update function_def.c
    Celcius To Kelvin function added

--- src/function_def.c
+++ src/function_def.c
@@ -1,16 +1,21 @@
 #include <function_def.h>
 
 float area_of_circle(int radius)
-  { 
-    ;return (3.14*radius*radius);
-  }
+{ 
+  return (3.14*radius*radius);
+}
 
 float area_of_triangle(int base, int height)
-  { 
-    return (0.5*base*height);
-  }
+{ 
+  return (0.5*base*height);
+}
 
 int area_of_square(int side_length)
-  { 
-    return (side_length*side_length);
-  }
+{ 
+  return (side_length*side_length);
+}
+
+int CelciusToKelvin(int temperature);
+{
+  return (temperature+273);
+}
