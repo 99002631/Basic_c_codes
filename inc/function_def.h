e38c9e3996bab5ef47fe5356df88121cc359ecfc
99002631 <71481233+99002631@users.noreply.github.com>
2020-09-19 10:36:48 +0530

    Create function_def.h
    function header added

---
+++ inc/function_def.h
@@ -0,0 +1,28 @@
+/** 
+* @file function_def.h
+* function definition header
+*
+*/
+#ifndef __FUNCTION_DEF_H__
+#define __FUNCTION_DEF_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+
+/**
+*  finds area of circle and returns the result
+*/
+float area_of_circle(int radius);
+
+/**
+*  finds the area of triangle and returns the result
+*/
+float area_of triangle(int base, int height);
+
+/**
+*  finds area of square and returns the result
+*/
+int area_of_square(int side_length);
+
+#endif  /* #define __FUNCTION_DEF_H__ */
